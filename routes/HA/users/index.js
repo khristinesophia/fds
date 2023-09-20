93c422ee64c33354802de80ef8395e9723ba1846
khrstn <khristine.sophia@gmail.com>
2023-09-20 13:37:07 +0800

    refactored edit user and change pass for superadmin FROM modal to SEPARATE PAGE

---
+++ routes/HA/users/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const pool = require('../../../config/db-config')
+
+const isAuthenticated = require('../../../middleware/isAuthenticated')
+
+
+
+
+module.exports = router
