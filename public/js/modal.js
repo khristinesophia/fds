22d1c71cd4ec52982e174c4b9818604835f299cf
MortemHugh <rickyintal11alpha@gmail.com>
2023-09-29 02:11:04 +0800

    Change the Edit and Change Password page into Modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -8,6 +8,8 @@ function closeModal() {
     modal.classList.remove('modal-active');
 
 }
+
+
 function openDeleteHotelModal(clickedElement) {
   var modal = document.getElementById('deletehotel');
   modal.classList.add('modal-active');
@@ -31,29 +33,65 @@ function closeDeleteHotelModal() {
 }
 
 
-  function confirmDelete() {
-    var checkbox = document.getElementById('confirm-checkbox');
-    var errorMessage = document.getElementById('checkbox-error-message');
-    
-    // Check if the checkbox is checked
-    if (checkbox.checked) {
-        // The checkbox is checked, proceed with the delete action
-        errorMessage.style.display = 'none'; // Hide the error message
-        // Continue with the delete action or form submission
-    } else {
-        // The checkbox is not checked, show an error message
-        errorMessage.style.display = 'block';
-        // Prevent the form from submitting
-        event.preventDefault();
-    }
+function openEditSAModal(clickedElement) {
+  var modal = document.getElementById('editsuper');
+  modal.classList.add('modal-active');
+
+  // Get the super admin data from data attributes
+  var userId = clickedElement.getAttribute('data-userid');
+  var fullName = clickedElement.getAttribute('data-fullname');
+  var username = clickedElement.getAttribute('data-username');
+
+  // Populate the form fields with the data from data attributes
+  var nameInput = document.querySelector('#editsuper input[name="name"]');
+  var usernameInput = document.querySelector('#editsuper input[name="username"]');
+  
+  if (nameInput) {
+    nameInput.value = fullName || '';
+  }
+
+  if (usernameInput) {
+    usernameInput.value = username || '';
+  }
+  
+  // Set the form action in the modal
+  var editForm = document.querySelector('#editsuper form');
+  if (editForm) {
+    editForm.action = `/superadmins/edit/${userId}`;
+  }
 }
+function closeEditSAModal() {
+  var modal = document.getElementById('editsuper');
+  modal.classList.remove('modal-active');
+}
+
+
+function openChangeSAModal(clickedElement) {
+  var modal = document.getElementById('changepassword');
+  modal.classList.add('modal-active');
+
+  // Get the user ID and password hash from data attributes
+  var userId = clickedElement.getAttribute('data-userid');
+  var passwordHash = clickedElement.getAttribute('data-password');
+
+  // Set the form action in the modal
+  var changePasswordForm = document.querySelector('#changepassword form');
+  if (changePasswordForm) {
+    changePasswordForm.action = `/superadmins/changepassword/${userId}`;
+  }
+}
+
+function closeChangeSAModal() {
+  var modal = document.getElementById('changepassword');
+  modal.classList.remove('modal-active');
+}
+
+// You can call openChangeSAModal(clickedElement) to open the modal.
 
 
 function openDeleteSuperModal(clickedElement) {
   var modal = document.getElementById('deletesuper');
   modal.classList.add('modal-active');
-
-
 
   // Get the hotel ID and hotel name from data attributes
   var userId = clickedElement.getAttribute('data-userid');
@@ -67,11 +105,25 @@ function openDeleteSuperModal(clickedElement) {
   var deleteForm = document.getElementById('delete-user-form');
   deleteForm.action = `/superadmins/delete/${userId}`;
 }
-
-
-
-
 function closeDeleteSuperModal() {
   var modal = document.getElementById('deletesuper');
   modal.classList.remove('modal-active');
+}
+
+
+function confirmDelete() {
+  var checkbox = document.getElementById('confirm-checkbox');
+  var errorMessage = document.getElementById('checkbox-error-message');
+  
+  // Check if the checkbox is checked
+  if (checkbox.checked) {
+      // The checkbox is checked, proceed with the delete action
+      errorMessage.style.display = 'none'; // Hide the error message
+      // Continue with the delete action or form submission
+  } else {
+      // The checkbox is not checked, show an error message
+      errorMessage.style.display = 'block';
+      // Prevent the form from submitting
+      event.preventDefault();
+  }
 }
