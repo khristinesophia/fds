9d6a685f3d93d62a136138eefedeba25adabd86e
khrstn <khristine.sophia@gmail.com>
2023-11-01 15:38:43 +0800

    archived ga folio done

--- routes/receptionist/archivedga/index.js
+++ routes/receptionist/archivedga/index.js
@@ -56,6 +56,88 @@ router.get('/', isAuthenticated, getHotelColor, async(req, res)=>{
 })
 
 
+//- render "folio" page
+//- "/ga/folio/:id"
+router.get('/folio/:id', isAuthenticated, getHotelColor, async(req, res)=>{
+
+    const hotelid = req.session.hotelID
+    const { id } = req.params
+
+
+    //- select from "hist_transactions" table (fds)
+    const q1 = `
+        SELECT * FROM hist_transactions
+        WHERE hotelid = $1
+        AND accountid = $2
+    `
+    const q1result = await pool.query(q1, [hotelid, id])
+
+
+    //- select from "hist_ancillary_transactions" table (ancillary)
+    const q2 = `
+        SELECT 
+            t1.ancillary_desc,
+            t2.ps_code,
+            t2.price,
+            t3.transaction_id,
+            t3.accountid,
+            t3.quantity,
+            t3.amount,
+            t3.paid
+        FROM ancillaries t1
+        JOIN product_service t2
+            ON t1.ancillary_id = t2.ancillary_id
+        JOIN hist_ancillary_transactions t3
+            ON t2.ps_id = t3.ps_id
+        WHERE t3.hotelid = $1
+        AND t3.accountid = $2
+    `
+    const q2result = await pool.query(q2, [hotelid, id])
+
+
+    //- select from "hist_housekeeping_transactions" table (housekeeping)
+    const q3 = `
+        SELECT 
+            t1.transactionid,
+            t1.paid,
+            t2.description,
+            t1.price,
+            t1.qty,
+            t1.amount
+        FROM hist_housekeeping_transactions t1
+        JOIN ref_items t2
+            ON t1.ref_itemid = t2.ref_itemid
+        WHERE t1.hotelid = $1 AND
+            t1.accountid = $2
+    `
+    const q3result = await pool.query(q3, [hotelid, id])
+
+
+    //- select from "ga" and "ga_gd" table (fds)
+    const q4 = `
+        SELECT *
+        FROM hist_guestaccounts t1
+            JOIN hist_guestaccounts_guestdetails t2
+                ON t1.accountid = t2.accountid
+            JOIN rooms t3
+                ON t1.roomid = t3.roomid
+        WHERE t1.hotelid = $1 AND 
+            t1.accountid = $2
+    `
+    const q4result = await pool.query(q4, [hotelid, id])
+
+
+    res.render('receptionist/archivedga/folio', {
+        hotelColor: req.hotelColor,
+        t1: q1result.rows,
+        t2: q2result.rows,
+        t3: q3result.rows,
+        ga: q4result.rows[0]
+    })
+
+})
+
+
 //- render "detail" page
 //- "/archivedga/detail/:id"
 router.get('/detail/:id', isAuthenticated, getHotelColor, async(req, res)=>{
