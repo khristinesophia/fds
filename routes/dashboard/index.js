334823ed6f8c3a6f7b1fdae570bd25401a0ac5af
MortemHugh <rickyintal11alpha@gmail.com>
2023-11-10 04:42:55 +0800

    Active account tracker in receptionist dashboard is made

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -1,18 +1,18 @@
-//- import path
+//- path import
 const path = require('path')
 
 //- express and router
 const express = require('express')
+const { type } = require('os')
 const router = express.Router()
 
-//- pool
+//- pool import
 const pool = require(path.join(__basedir, 'config', 'db-config'))
 
 //- middlewares
 const isAuthenticated = require(path.join(__basedir, 'middleware', 'isAuthenticated'))
 const getHotelColor = require(path.join(__basedir, 'middleware', 'getHotelColor'))
 const getHotelLogo = require(path.join(__basedir, 'middleware', 'getHotelLogo'))
-
 
 
 
@@ -26,13 +26,31 @@ router.get('/hsadmin', isAuthenticated, getHotelColor, getHotelLogo, async(req,r
 })
 
 // render R dashboard
-router.get('/receptionist', isAuthenticated, getHotelColor, getHotelLogo, async (req,res)=>{
-    
-    res.render('dashboard/receptionist',{
-        hotelColor: req.hotelColor,
-        hotelLogo: req.hotelImage
-    })
-})
+// render R dashboard
+router.get('/receptionist', isAuthenticated, getHotelColor, getHotelLogo, async (req, res) => {
+    const hotelid = req.session.hotelID
+
+    // Query to get the count of active guest accounts
+    const qCount = `
+        SELECT COUNT(*) FROM guestaccounts WHERE hotelid = $1
+    `;
+    try {
+        const countResult = await pool.query(qCount, [hotelid]);
+        const guestCount = countResult.rows[0].count; // Assuming COUNT(*) returns a count property
+
+        res.render('dashboard/receptionist', {
+            hotelColor: req.hotelColor,
+            hotelLogo: req.hotelImage,
+            guestCount: guestCount // Pass the count to the dashboard template
+        });
+    } catch (error) {
+        console.error("Error fetching guest count", error);
+        res.sendStatus(500); // or render an error page, handle as needed
+    }
+});
+
+
+
 
 
 
