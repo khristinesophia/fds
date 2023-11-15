dff93f679f5fec0cf9d2726e79d016d86837f386
khrstn <khristine.sophia@gmail.com>
2023-11-15 11:06:55 +0800

    added and fixed utils/formatDateWithTime

--- utils/formatDateWithTime.js
+++ utils/formatDateWithTime.js
@@ -5,15 +5,14 @@ function formatDateWithTime(d){
     const day = date.getDate().toString().padStart(2, "0")
     let hours = date.getHours().toString().padStart(2, "0")
     const minutes = date.getMinutes().toString().padStart(2, "0")
-    // const seconds = date.getSeconds().toString().padStart(2, "0")
 
     let ampm = hours >= 12 ? 'PM' : 'AM'
 
      // Convert hours to 12-hour format
-    hours = hours % 12;
+    hours = hours % 12
 
     // Convert '0' to '12'
-    hours = hours ? hours : 12;
+    hours = hours ? hours : 12
  
     const formattedDate = `${year}-${month}-${day} ${hours}:${minutes} ${ampm}`
  
