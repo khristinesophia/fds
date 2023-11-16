5d9a156d15ba0817921b6e5f6206233f44e8d3ed
MortemHugh <rickyintal11alpha@gmail.com>
2023-11-17 03:39:27 +0800

    add total to the graph

--- public/js/chartScript.js
+++ public/js/chartScript.js
@@ -1,25 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var ctx = document.getElementById('donutChart').getContext('2d');
-  
-    var data = {
-      labels: ['Adult', 'Children'],
-      datasets: [{
-        data: [adultNoCount, childNoCount],
-        backgroundColor: ['#36a2eb', '#ff6384'],
-        borderWidth: 1
-      }]
-    };
-  
-    var options = {
-      cutoutPercentage: 70,
-      responsive: true,
-      maintainAspectRatio: false,
-    };
-  
-    var myDoughnutChart = new Chart(ctx, {
-      type: 'doughnut',
-      data: data,
-      options: options
-    });
+  var ctx = document.getElementById('donutChart').getContext('2d');
+
+  var data = {
+    labels: ['Adult', 'Children'],
+    datasets: [{
+      data: [adultNoCount, childNoCount],
+      backgroundColor: ['#36a2eb', '#ff6384'],
+      borderWidth: 1
+    }]
+  };
+
+  var options = {
+    cutoutPercentage: 70,
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+
+  var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: data,
+    options: options
   });
-  
+
+  // Calculate total sum and display it
+  var totalCount = adultNoCount + childNoCount;
+  document.getElementById('totalCount').textContent = 'Total: ' + totalCount;
+});
