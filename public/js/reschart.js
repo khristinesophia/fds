97d883fd3121955eafe509829635925054c3ab79
MortemHugh <rickyintal11alpha@gmail.com>
2023-11-24 03:10:19 +0800

    color set is predefined, 15 set color only so no light color will be shown

--- public/js/reschart.js
+++ public/js/reschart.js
@@ -1,71 +1,59 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    
-    var roomTypeLabels = reservations.map(function(r) {
-        return r.roomtype;
-      });
-  
-      var roomTypeCounts = reservations.map(function(r) {
-        return r.roomtype_count;
-      });
-      
-      var totalReservations = roomTypeCounts.reduce(function(acc, count) {
-        return acc + count;
-      }, 0);
-  
-      document.getElementById('reservationCount').textContent = 'Total Reservations: ' + totalReservations;
-  
-      var ctxReservation = document.getElementById('reservationDonutChart').getContext('2d');
-  
-      var dataReservation = {
-        labels: roomTypeLabels,
-        datasets: [{
+
+  var roomTypeLabels = reservations.map(function(r) {
+      return r.roomtype;
+  });
+
+  var roomTypeCounts = reservations.map(function(r) {
+      return r.roomtype_count;
+  });
+
+  var totalReservations = roomTypeCounts.reduce(function(acc, count) {
+      return acc + count;
+  }, 0);
+
+  document.getElementById('reservationCount').textContent = 'Total Reservations: ' + totalReservations;
+
+  var ctxReservation = document.getElementById('reservationDonutChart').getContext('2d');
+
+  // Define an array of 15 dark colors
+  var predefinedColors = [
+      '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+      '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',
+      '#334455', '#8899aa', '#dd4477', '#117733', '#aa88cc'
+  ];
+
+  var dataReservation = {
+      labels: roomTypeLabels,
+      datasets: [{
           data: roomTypeCounts,
-          backgroundColor: getRandomColorsDark(roomTypeLabels.length),
+          backgroundColor: predefinedColors,
           borderWidth: 1
-        }]
-      };
-  
-      var optionsReservation = {
-        cutoutPercentage: 70,
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
+      }]
+  };
+
+  var optionsReservation = {
+      cutoutPercentage: 70,
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
           legend: {
-            position: 'right',
-            labels: {
-                font: {
-                  size: 14,
-                  weight: 'bold' 
-                }
-            } 
+              position: 'right',
+              labels: {
+                  font: {
+                      size: 14,
+                      weight: 'bold'
+                  }
+              }
           },
-        },
-      };
-  
-      var myReservationDoughnutChart = new Chart(ctxReservation, {
-        type: 'doughnut',
-        data: dataReservation,
-        options: optionsReservation
-      });
-  
-    });
-  
-    // Function to generate random colors
-    function getRandomColorsDark(count) {
-      var colors = [];
-      for (var i = 0; i < count; i++) {
-        colors.push(getRandomDarkColor());
-      }
-      return colors;
-    }
-  
-    // Function to generate a random color
-    function getRandomDarkColor() {
-      var letters = '0123456789ABCDEF';
-      var color = '#';
-      for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    }
+      },
+  };
+
+  var myReservationDoughnutChart = new Chart(ctxReservation, {
+      type: 'doughnut',
+      data: dataReservation,
+      options: optionsReservation
+  });
+
+});
