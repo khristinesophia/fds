f0ffd851669111b8758feae8985b05005ed49a10
MortemHugh <rickyintal11alpha@gmail.com>
2023-11-24 00:25:15 +0800

    Donut for reservation is almost done

---
+++ public/js/reschart.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    
+    var roomTypeLabels = reservations.map(function(r) {
+        return r.roomtype;
+      });
+  
+      var roomTypeCounts = reservations.map(function(r) {
+        return r.roomtype_count;
+      });
+      
+      var totalReservations = roomTypeCounts.reduce(function(acc, count) {
+        return acc + count;
+      }, 0);
+  
+      document.getElementById('reservationCount').textContent = 'Total Reservations: ' + totalReservations;
+  
+      var ctxReservation = document.getElementById('reservationDonutChart').getContext('2d');
+  
+      var dataReservation = {
+        labels: roomTypeLabels,
+        datasets: [{
+          data: roomTypeCounts,
+          backgroundColor: getRandomColorsDark(roomTypeLabels.length),
+          borderWidth: 1
+        }]
+      };
+  
+      var optionsReservation = {
+        cutoutPercentage: 70,
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            position: 'right',
+            labels: {
+                font: {
+                  size: 14, // Set the font size
+                  weight: 'bold' // Set the font weight
+                }
+            } // Change to 'left' if you want it on the left side
+          },
+        },
+      };
+  
+      var myReservationDoughnutChart = new Chart(ctxReservation, {
+        type: 'doughnut',
+        data: dataReservation,
+        options: optionsReservation
+      });
+  
+    });
+  
+    // Function to generate random colors
+    function getRandomColorsDark(count) {
+      var colors = [];
+      for (var i = 0; i < count; i++) {
+        colors.push(getRandomDarkColor());
+      }
+      return colors;
+    }
+  
+    // Function to generate a random color
+    function getRandomDarkColor() {
+      var letters = '0123456789ABCDEF';
+      var color = '#';
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
