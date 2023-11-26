e44fbeac6fba89fcc3dfb0efad33754c5b360fff
MortemHugh <rickyintal11alpha@gmail.com>
2023-11-26 19:56:02 +0800

    Dashboard for coutns

--- config/db-config.js
+++ config/db-config.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'acis',
-    host: 'localhost',
+    user: 'acis',
+    password: 'Ofdvpi7BxtIJ9KwisfYOc6nGb5HWAbLN',
+    host: 'dpg-clha9hmbbf9s73b0brp0-a',
     port: 5432, 
     database: 'fds'
 })
 
-module.exports = pool; 
+module.exports = pool;
