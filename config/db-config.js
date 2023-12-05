989fa409734d5130a90b73066dc329fbe0797cf5
khrstn <khristine.sophia@gmail.com>
2023-12-05 13:31:26 +0800

    fixed db config

--- config/db-config.js
+++ config/db-config.js
@@ -1,35 +1,21 @@
 const Pool = require('pg').Pool
 
-<<<<<<< HEAD
-// const pool = new Pool({
-//     user: 'acis',
-//     password: 'Ofdvpi7BxtIJ9KwisfYOc6nGb5HWAbLN',
-//     host: 'dpg-clha9hmbbf9s73b0brp0-a',
-//     port: 5432, 
-//     database: 'fds'
-// })
-
-
-=======
->>>>>>> main
 const pool = new Pool({
-    user: 'postgres',
-    password: 'acis',
-    host: 'localhost',
+    user: 'acis',
+    password: 'Ofdvpi7BxtIJ9KwisfYOc6nGb5HWAbLN',
+    host: 'dpg-clha9hmbbf9s73b0brp0-a',
     port: 5432, 
     database: 'fds'
 })
 
-<<<<<<< HEAD
-=======
+
 // const pool = new Pool({
-//     user: 'acis',
-//     password: 'Ofdvpi7BxtIJ9KwisfYOc6nGb5HWAbLN',
-//     host: 'dpg-clha9hmbbf9s73b0brp0-a',
+//     user: 'postgres',
+//     password: 'acis',
+//     host: 'localhost',
 //     port: 5432, 
 //     database: 'fds'
 // })
 
->>>>>>> main
 
 module.exports = pool;
